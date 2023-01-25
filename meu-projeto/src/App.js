ec201f769a83ff88e49d0bc447c43822abc435ad
Gabriela Caruso <gabriela.caruso@seriedesign.com.br>
2023-01-25 19:23:01 -0300

    Inserindo CSS no React

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase/>
+      <Frase/>
       <SayMyName nome="Gabriela"/>
       <SayMyName nome="Maurici"/>
       <SayMyName nome="Romena"/>

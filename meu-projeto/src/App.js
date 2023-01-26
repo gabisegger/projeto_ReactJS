98e6f04718616141129193cf000ad2da07e34125
Gabriela Caruso <gabriela.caruso@seriedesign.com.br>
2023-01-26 08:12:46 -0300

    Utilizando React fragments

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <SayMyName nome="Maurici"/>
       <SayMyName nome="Romena"/>
       <Pessoa nome="Gabriela" idade="25" profissao="Programador" foto="https://via.placeholder.com/150"/>
+      <List/>
     </div>
   );
 }

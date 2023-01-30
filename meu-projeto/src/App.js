63c4217c36906763e40bb05a7e0e7524a776e37b
Gabriela Caruso <gabriela.caruso@seriedesign.com.br>
2023-01-30 08:45:04 -0300

    Eventos no React (onClick, onChange e onSubmit)

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,20 +1,14 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <h1>Testando CSS</h1>
-      <Frase/>
-      <Frase/>
-      <SayMyName nome="Gabriela"/>
-      <SayMyName nome="Maurici"/>
-      <SayMyName nome="Romena"/>
-      <Pessoa nome="Gabriela" idade="25" profissao="Programador" foto="https://via.placeholder.com/150"/>
-      <List/>
+      <Evento numero= '1' />
+      <Evento numero= '2' />
+      <Form/>
     </div>
   );
 }
